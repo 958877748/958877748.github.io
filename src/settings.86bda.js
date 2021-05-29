a40f3389893ded2385699a6a722a065d8a6a0596
gl <gltxdy@outlook.com>
2021-05-29 10:28:20 +0800

    init

---
+++ src/settings.86bda.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/a.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"137d9"}};
